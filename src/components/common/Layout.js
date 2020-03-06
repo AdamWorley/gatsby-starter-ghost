ee6d865227819ffa2f5f205d604d04abac8e99ac
adamworley <worley9@hotmail.co.uk>
2020-03-06 22:55:34 +0000

    Change header and footer

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -29,15 +29,17 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                 <html lang={site.lang} />
                 <style type="text/css">{`${site.codeinjection_styles}`}</style>
                 <body className={bodyClass} />
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
             </Helmet>
 
             <div className="viewport">
 
                 <div className="viewport-top">
                     {/* The main header section on top of the screen */}
-                    <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
+                    {/* <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}> */}
+                    <header className="site-head">
                         <div className="container">
-                            <div className="site-mast">
+                            {/* <div className="site-mast">
                                 <div className="site-mast-left">
                                     <Link to="/">
                                         {site.logo ?
@@ -47,17 +49,19 @@ const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
                                     </Link>
                                 </div>
                                 <div className="site-mast-right">
+                                {/* <div><a className="site-nav-item" href="tel:07849838319"><i class="fa fa-phone"></i> 07849838319</a>
+                            <a className="site-nav-item" href={ facebookUrl }><i class="fa fa-facebook"></i> Facebook</a>
+                            <a className="site-nav-item" href="mailto:lauren@crawleydogwalkers.com"><i class="fa fa-envelope"></i> Email</a> 
+                            </div> /}
                                     { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
                                     { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
                                     <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
                                 </div>
-                            </div>
-                            { isHome ?
-                                <div className="site-banner">
-                                    <h1 className="site-banner-title">{site.title}</h1>
-                                    <p className="site-banner-desc">{site.description}</p>
-                                </div> :
-                                null}
+                            </div> */}
+                            <div class="site-banner">
+                            <img src={site.cover_image}/>
+                            <p className="site-banner-desc">{site.description}</p>
+                           </div>
                             <nav className="site-nav">
                                 <div className="site-nav-left">
                                     {/* The navigation items as setup in Ghost */}
